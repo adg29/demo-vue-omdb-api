98fabf898f6c12c137bc5e2f6ceecf205036fdbe
Alan Garcia <alandavidgarcia@gmail.com>
2020-04-05 16:06:10 -0400

    latestmovie view and router

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import LatestMovie from '@/components/LatestMovie'
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'LatestMovie',
+    component: LatestMovie
   },
   {
     path: '/about',
