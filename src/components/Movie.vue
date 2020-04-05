eddb7ddabdd30cfabc15fe350f390b0d72baa930
Alan Garcia <alandavidgarcia@gmail.com>
2020-04-05 19:51:22 -0400

    ratings in movie component

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -8,6 +8,51 @@
                 <pre>
                     {{JSON.stringify(singleMovie, null, 5)}}
                 </pre>
+                <div class="text-xs-center">
+                    <v-dialog
+                        v-model="dialog"
+                        width="500"
+                    >
+                        <v-btn
+                            slot="activator"
+                            color="green"
+                            dark
+                        >
+                            View Ratings
+                        </v-btn>
+                        <v-card>
+                            <v-card-title
+                                class="headline grey lighten-2"
+                                primary-title
+                            >
+                                Ratings
+                            </v-card-title>
+                            <v-card-text>
+                                <table style="width:100%" border="1" >
+                                    <tr>
+                                        <th>Source</th>
+                                        <th>Ratings</th>
+                                    </tr>
+                                    <tr v-for="(rating, index) in this.ratings" :key="index">
+                                        <td align="center">{{rating.Source}}</td>
+                                        <td align="center">{{rating.Value}}</td>
+                                    </tr>
+                                </table>
+                            </v-card-text>
+                            <v-divider></v-divider>
+                            <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn
+                                color="primary"
+                                flat
+                                @click="dialog = false"
+                            >
+                                OK
+                            </v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-dialog>
+                </div>
             </v-flex>
         </v-layout>
     </v-container>
@@ -19,7 +64,10 @@ export default {
     props: ['id'],
     data() {
         return {
-            singleMovie: ''
+            singleMovie: '',
+            dialog: false,
+            loading: true,
+            ratings: ''
         }
     },
     mounted() {
